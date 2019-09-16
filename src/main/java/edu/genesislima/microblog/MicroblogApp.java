2209f041159fa4dfba2ebf9b24f0e8a8b16a1a8f
Genesis Pereira <profgenesislima@gmail.com>
2019-09-16 19:47:23 -0300

    initial commit for Microblog.

---
+++ src/main/java/edu/genesislima/microblog/MicroblogApp.java
@@ -0,0 +1,16 @@
+package edu.genesislima.microblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+//@EnableWebMvc
+public class MicroblogApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroblogApp.class, args);
+
+	}
+
+}
